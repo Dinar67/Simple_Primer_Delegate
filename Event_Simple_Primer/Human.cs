dd620031535364b55ac004dde619aada2fcebb43
User <User@DESKTOP-GLMVQT8>
2024-01-18 12:34:17 +0300

    Add existing file

---
+++ Event_Simple_Primer/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Simple_Primer
+{
+    public delegate void ChangeHealth(int Health);
+    internal class Human
+    {
+        private int _health;
+        private int _maxHealth;
+
+        public Human(int health)
+        {
+            //fff
+            _health = health;
+            _maxHealth = Health;
+        }
+
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                if(value < 0)
+                {
+                    _health = 0;
+                }
+                else if(value > _maxHealth)
+                {
+                    _health = _maxHealth;
+                }
+                else
+                {
+                    _health = value;
+                }
+                OnChangeHealth.Invoke(value < 0? 0 : value);
+
+            }
+        }
+
+        public int MaxHealth
+        {
+            get { return _maxHealth; }
+        }
+
+        public event ChangeHealth OnChangeHealth;
+    }
+}
---
+++ Event_Simple_Primer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Event_Simple_Primer
+{
+    class Programm
+    {
+        static void Main()
+        {
+            Human human = new Human(100);
+            human.OnChangeHealth += GetHealth;
+
+            human.Health -= 10;
+            human.Health -= 100;
+
+
+
+        }
+
+        public static void GetHealth(int Health) 
+        {
+            Console.WriteLine($"Health: {Health}");
+        }
+    }
+}
